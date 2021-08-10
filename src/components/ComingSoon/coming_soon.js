ad339c5896cf7936efafd72e97293a6fddb1f8b4
Shreyas kumar <kumarshreyas073@gmail.com>
2021-08-10 20:49:27 +0530

    Initial commit

---
+++ src/components/ComingSoon/coming_soon.js
@@ -0,0 +1,9 @@
+import './coming_soon.css';
+
+function ComingSoon(){
+    return(
+        <h1>Coming Soon....</h1>
+    );
+}
+
+export default ComingSoon;
---
+++ src/components/Section2/section2.js
@@ -0,0 +1,12 @@
+import ComingSoon from '../ComingSoon/coming_soon';
+import './section2.css';
+
+function Section2(){
+    return(
+        <div className = "section-2-main">
+            <ComingSoon/>
+        </div>
+    );
+}
+
+export default Section2;
